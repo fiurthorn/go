fa7702a00854ed2592238a9185fb74ec529dc1ea
Stefan Weinmann <stefan@firthorn.de>
2021-10-12 19:55:49 +0200

    removed dart elevator

--- cmd/elevator/elevator.go
+++ cmd/elevator/elevator.go
@@ -25,46 +25,25 @@ type Ref struct {
 	} `json:"object"`
 }
 
-func githubUserProject(golang bool) (string, string) {
-	if golang {
-		return "golang", "go"
-	}
-	return "dart-lang", "sdk"
+func githubUserProject() (string, string) {
+	return "golang", "go"
 }
 
-func githubApiUrl(golang bool) string {
-	user, project := githubUserProject(golang)
+func githubApiUrl() string {
+	user, project := githubUserProject()
 	return fmt.Sprintf("https://api.github.com/repos/%s/%s/git/refs/tags", user, project)
 }
 
-func versionExtractPrefix(golang bool) string {
-	if golang {
-		return "go"
-	}
-	return ""
+func versionExtractPrefix() string {
+	return "go"
 }
 
-func versionExtractRegex(golang bool) *regexp.Regexp {
-	return regexp.MustCompile(fmt.Sprintf(`^refs/tags/(%s\d+(\.\d+(\.\d+)?)?)$`, versionExtractPrefix(golang)))
+func versionExtractRegex() *regexp.Regexp {
+	return regexp.MustCompile(fmt.Sprintf(`^refs/tags/(%s\d+(\.\d+(\.\d+)?)?)$`, versionExtractPrefix()))
 }
 
 func convert(output []byte, err error) (string, error) {
 	return string(output), err
-}
-
-func currentDartVersion() string {
-	executable, err := exec.LookPath("dart")
-	if err != nil {
-		return "0"
-	}
-	proc := exec.Cmd{Path: executable, Args: []string{executable, "--version"}}
-	outputString, err := convert(proc.CombinedOutput())
-	if err != nil {
-		log.Panic(err.Error())
-	}
-	outputString = strings.TrimPrefix(string(outputString), "Dart SDK version: ")
-	outputString = strings.Split(outputString, " ")[0]
-	return outputString
 }
 
 func currentGoVersion() string {
@@ -82,45 +61,15 @@ func currentGoVersion() string {
 
 }
 
-func currentVersion(golang bool) string {
-	if golang {
-		return currentGoVersion()
-	}
-
-	return currentDartVersion()
+func currentVersion() string {
+	return currentGoVersion()
 }
 
-func downloadUrl(golang bool, version string) (url string, filename string) {
-	if golang {
-		filename = fmt.Sprintf(`%s.%s-%s.%s`, version, runtime.GOOS, runtime.GOARCH, goLangExtention())
-		log.Println("select:", filename)
-		url = fmt.Sprintf(`https://golang.org/dl/%s`, filename)
-		return
-	}
-
-	filename = fmt.Sprintf("dartsdk-%s-%s-%s.zip", dartOs(), dartArch(), version)
-	url = fmt.Sprintf("https://storage.googleapis.com/dart-archive/channels/stable/release/%s/sdk/dartsdk-%s-%s-release.zip", version, dartOs(), dartArch())
+func downloadUrl(version string) (url string, filename string) {
+	filename = fmt.Sprintf(`%s.%s-%s.%s`, version, runtime.GOOS, runtime.GOARCH, goLangExtention())
+	log.Println("select:", filename)
+	url = fmt.Sprintf(`https://golang.org/dl/%s`, filename)
 	return
-}
-
-func dartOs() string {
-	switch runtime.GOOS {
-	case "darwin":
-		return "macos"
-	default:
-		return runtime.GOOS
-	}
-}
-
-func dartArch() string {
-	switch runtime.GOARCH {
-	case "amd64":
-		return "x64"
-	case "386":
-		return "ia32"
-	default:
-		return runtime.GOARCH
-	}
 }
 
 func goLangExtention() string {
@@ -138,16 +87,11 @@ func goLangExtention() string {
 }
 
 func main() {
-	args := os.Args[1:]
-	golang := true
-	if len(args) > 0 && args[0] == "dart" {
-		golang = false
-	}
 	refs := &[]Ref{}
 	client := &http.Client{}
 
 	log.Println("get github tags")
-	req, err := http.NewRequest("GET", githubApiUrl(golang), nil)
+	req, err := http.NewRequest("GET", githubApiUrl(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -159,7 +103,7 @@ func main() {
 	json.NewDecoder(resp.Body).Decode(refs)
 
 	log.Println("filter github tags")
-	validRef := versionExtractRegex(golang)
+	validRef := versionExtractRegex()
 	extract := make([]string, 0, len(*refs))
 	for _, ref := range *refs {
 		if validRef.MatchString(ref.Ref) {
@@ -171,11 +115,11 @@ func main() {
 	extractVersion := extract[len(extract)-1]
 	log.Println("filtered:", extractVersion)
 
-	log.Printf("extractVersion:%s, currentVersion:%s", extractVersion, currentVersion(golang))
-	if currentVersion(golang) == extractVersion {
-		log.Printf("latest version already installed (%s==%s)", extractVersion, currentVersion(golang))
+	log.Printf("extractVersion:%s, currentVersion:%s", extractVersion, currentVersion())
+	if currentVersion() == extractVersion {
+		log.Printf("latest version already installed (%s==%s)", extractVersion, currentVersion())
 	} else {
-		url, filename := downloadUrl(golang, extractVersion)
+		url, filename := downloadUrl(extractVersion)
 		log.Print(url)
 
 		res, err := client.Get(url)
@@ -200,40 +144,19 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if strings.HasSuffix(filename, ".msi") {
-			installMsi(fullpath)
-		} else {
-			executable, err := lookupExecutable(golang)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			dirname := filepath.Dir(executable)
-			if strings.HasSuffix(dirname, "bin") {
-				dirname = filepath.Dir(executable)
-			}
-			log.Printf("install location: %s => %s", fullpath, dirname)
+		executable, err := lookupExecutable()
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		dirname := filepath.Dir(executable)
+		if strings.HasSuffix(dirname, "bin") {
+			dirname = filepath.Dir(executable)
+		}
+		log.Printf("install location: %s => %s", fullpath, dirname)
 	}
 }
 
-func lookupExecutable(golang bool) (string, error) {
-	if golang {
-		return exec.LookPath("go")
-	}
-
-	return exec.LookPath("dart")
-}
-
-func installMsi(fullpath string) {
-	msiexec, err := exec.LookPath("msiexec")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("%s %s", msiexec, fullpath)
-	_, err = exec.Command(msiexec, "/package", fullpath).CombinedOutput()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func lookupExecutable() (string, error) {
+	return exec.LookPath("go")
 }
