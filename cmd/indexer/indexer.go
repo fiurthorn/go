7ac48b946b5b4400e2217ba8eeddeab86825cbdb
Stefan Weinmann <s.weinmann@intex-online.de>
2021-12-06 22:32:00 +0100

    key value db badgerhold

--- cmd/indexer/indexer.go
+++ cmd/indexer/indexer.go
@@ -10,22 +10,61 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/fiurthorn/go/lib"
+	bh "github.com/timshannon/badgerhold/v4"
 	"golang.org/x/crypto/sha3"
 )
 
+type Entry struct {
+	ID   uint64 `badgerhold:"key"`
+	Hash string `badgerhold:"index"`
+
+	Files *lib.StringSet
+}
+
 var wg = sync.WaitGroup{}
 var pool = make(chan string, 64)
+var store *bh.Store
 
 func main() {
-	for i := 0; i < 32; i++ {
+	for i := 0; i < 4; i++ {
 		wg.Add(1)
 		go worker(i, pool)
 	}
-	filepath.WalkDir("c:/Users/s.weinmann/workspace", walker)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Panic(err)
+	}
+	dir := filepath.Join(home, "indexer")
+
+	options := bh.DefaultOptions
+	options.Dir = dir
+	options.ValueDir = dir
+
+	store, err = bh.Open(options)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer store.Close()
+
+	filepath.WalkDir("/home/fiurthorn/workspace/xxo", walker)
 	log.Println("walked through")
 	close(pool)
 	log.Println("waiting")
 	wg.Wait()
+
+	results := []Entry{}
+	store.Find(&results, (*bh.Query)(bh.Where("Files").MatchFunc(func(ra *bh.RecordAccess) (bool, error) {
+		if files, ok := ra.Field().(*lib.StringSet); ok {
+			return files.Len() > 1, nil
+		}
+		return false, nil
+	})))
+
+	for _, result := range results {
+		log.Println(result)
+	}
+	log.Println(len(results))
 }
 
 func walker(path string, d fs.DirEntry, err error) error {
@@ -54,6 +93,23 @@ func worker(i int, pool chan string) {
 func logHash(i int, path string) string {
 	hash := calcHash(path)
 	log.Printf("[%2d]: %s: %s\r", i, hash, path)
+
+	var result Entry
+	store.FindOne(&result, (*bh.Query)(bh.Where("Hash").Eq(hash)))
+	if result.ID > 0 || (result.Files != nil && result.Files.Len() > 0) {
+		if has := result.Files.Has(path); !has {
+			result.Files.Add(path)
+			store.Update(result.ID, result)
+		}
+		return hash
+	}
+
+	data := Entry{
+		Hash:  hash,
+		Files: lib.NewStringSetWith(path),
+	}
+	store.Insert(bh.NextSequence(), data)
+
 	return hash
 }
 
--- go.mod
+++ go.mod
@@ -4,17 +4,34 @@ go 1.17
 
 require (
 	github.com/rivo/tview v0.0.0-20211001102648-5508f4b00266
+	github.com/timshannon/badgerhold/v4 v4.0.1
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
 require (
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dgraph-io/badger/v3 v3.2103.1 // indirect
+	github.com/dgraph-io/ristretto v0.1.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/glog v0.0.0-20210429001901-424d2337a529 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/flatbuffers v2.0.0+incompatible // indirect
+	github.com/klauspost/compress v1.13.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.14-0.20210830053702-dc8fe66265af // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
