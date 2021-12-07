e1cefe2c7fca25101ad5597ed95a136c11b8ed54
Weinmann <s.weinmann@intex-online.de>
2021-12-07 16:39:40 +0100

    handle CTRL+C

--- cmd/indexer/indexer.go
+++ cmd/indexer/indexer.go
@@ -8,29 +8,49 @@ import (
 	"io/fs"
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/fiurthorn/go/lib"
 	bh "github.com/timshannon/badgerhold/v4"
 
 	// bh "github.com/timshannon/bolthold"
+
 	"golang.org/x/crypto/sha3"
 )
 
-type Entry struct {
+type HashEntry struct {
 	ID   uint64 `badgerhold:"key" boltholdKey:"ID"`
 	Hash string `badgerhold:"index" boltholdIndex:"Hash"`
 
 	Files *lib.StringSet
 }
 
+type FileEntry struct {
+	ID   string `badgerhold:"key" boltholdKey:"ID"`
+	Size int64  `badgerhold:"index" boltholdIndex:"Size"`
+}
+
 var wg = sync.WaitGroup{}
 var pool = make(chan string, 64)
 var store *bh.Store
+var quit = false
+
+func signalHandler(cancel chan os.Signal) {
+	signal.Notify(cancel, syscall.SIGTERM, syscall.SIGINT)
+
+	for signal := range cancel {
+		log.Print("terminate:", signal)
+		quit = true
+	}
+}
 
 func main() {
+	go signalHandler(make(chan os.Signal))
+
 	start := time.Now()
 	go inserter()
 	for i := 0; i < 4; i++ {
@@ -57,7 +77,7 @@ func main() {
 	}
 	defer store.Close()
 
-	filepath.WalkDir(filepath.Join(home, "workspace"), walker)
+	filepath.WalkDir(filepath.Join(home, "Downloads"), walker)
 	log.Println("walked through")
 	close(pool)
 	log.Println("waiting")
@@ -66,26 +86,32 @@ func main() {
 	<-ready
 	d := time.Since(start)
 
-	results := []Entry{}
-	err = store.Find(&results, (*bh.Query)(bh.Where("Files").MatchFunc(func(ra *bh.RecordAccess) (bool, error) {
-		if files, ok := ra.Field().(*lib.StringSet); ok {
-			return files.Len() > 1, nil
+	if !quit {
+		results := []HashEntry{}
+		err = store.Find(&results, (*bh.Query)(bh.Where("Files").MatchFunc(func(ra *bh.RecordAccess) (bool, error) {
+			if files, ok := ra.Field().(*lib.StringSet); ok {
+				return files.Len() > 1, nil
+			}
+			return false, nil
+		})))
+		if err != nil {
+			log.Panic(err)
 		}
-		return false, nil
-	})))
-	if err != nil {
-		log.Panic(err)
-	}
 
-	for _, result := range results {
-		log.Println(result)
-	}
+		for _, result := range results {
+			log.Println(result)
+		}
 
-	count, err := store.Count(Entry{}, &bh.Query{})
-	if err != nil {
-		log.Panic(err)
+		count, err := store.Count(HashEntry{}, &bh.Query{})
+		if err != nil {
+			log.Panic(err)
+		}
+		count2, err := store.Count(FileEntry{}, &bh.Query{})
+		if err != nil {
+			log.Panic(err)
+		}
+		log.Println(len(results), "/", count, "/", count2)
 	}
-	log.Println(len(results), "/", count)
 	log.Println(insert, "/", visit)
 	log.Println("time", d)
 }
@@ -93,6 +119,7 @@ func main() {
 type FileHash struct {
 	Hash string
 	File string
+	Size int64
 }
 
 var queue = make(chan FileHash, 20)
@@ -102,23 +129,25 @@ var insert = 0
 func inserter() {
 	for entry := range queue {
 		insert++
-		var result Entry
+		var result HashEntry
 		store.FindOne(&result, (*bh.Query)(bh.Where("Hash").Eq(entry.Hash).Index("Hash")))
 		if result.ID > 0 || (result.Files != nil && result.Files.Len() > 0) {
 			if has := result.Files.Has(entry.File); !has {
 				result.Files.Add(entry.File)
 				// log.Println("update", entry.File)
 				store.Update(result.ID, result)
+				store.Insert(entry.File, FileEntry{Size: entry.Size})
 			}
 			continue
 		}
 
-		data := Entry{
+		data := HashEntry{
 			Hash:  entry.Hash,
 			Files: lib.NewStringSetWith(entry.File),
 		}
 		// log.Println("insert", entry.File)
 		store.Insert(bh.NextSequence(), data)
+		store.Insert(entry.File, FileEntry{Size: entry.Size})
 	}
 	ready <- struct{}{}
 }
@@ -135,8 +164,10 @@ func walker(path string, d fs.DirEntry, err error) error {
 	}
 
 	if d.Type().IsRegular() {
-		visit++
-		pool <- path
+		if !quit {
+			visit++
+			pool <- path
+		}
 	}
 
 	return nil
@@ -144,26 +175,32 @@ func walker(path string, d fs.DirEntry, err error) error {
 
 func worker(i int, pool chan string, h hash.Hash) {
 	for path := range pool {
-		logHash(i, path, h)
+		if !quit {
+			logHash(i, path, h)
+		}
 	}
 	wg.Done()
 }
 
 func logHash(i int, path string, h hash.Hash) string {
-	hash := calcHash(path, h)
-	log.Printf("[%2d]: %s: %s\r", i, hash, path)
-	queue <- FileHash{Hash: hash, File: path}
+	start := time.Now()
+	hash, size := calcHash(path, h)
+	log.Printf("[%2d]: [%10s] %s: %s\r", i, time.Since(start), hash, path)
+	queue <- FileHash{Hash: hash, File: path, Size: size}
 	return hash
 }
 
-func calcHash(path string, h hash.Hash) string {
+func calcHash(path string, h hash.Hash) (string, int64) {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Printf("skip dir '%s' with error: %v", path, err)
 	}
 	defer file.Close()
 
+	stat, err := file.Stat()
+	size := stat.Size()
+
 	h.Reset()
 	io.Copy(h, file)
-	return hex.EncodeToString(h.Sum(nil))
+	return hex.EncodeToString(h.Sum(nil)), size
 }
